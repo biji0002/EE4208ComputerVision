45524fce5b586765f52845685e7199a11bae9e3f
jujuejundao <xu325504@hotmail.com>
2019-02-28 19:14:23 -0800

    1

---
+++ CVpro1/main.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy
+import math
+from PIL import Image
+import zncc
+import pca
+import base_faces_lib as lib
+
+
+#import the mean adjusted image base matrix
+base_matrix_normalised = lib.ini_img_base()
+
+#input the test image
+img = Image.open('3.png').convert('L')  #import the image base
+img_arr_2d = numpy.asarray(img)              #convert the image to 2d array
+img_arr_1d = img_arr_2d.flatten()
+test_array = numpy.zeros(len(img_arr_1d))
+for x in range(0, len(img_arr_1d)):
+	test_array[x] = img_arr_1d[x]
+
+#Perform zncc test
+#return the image no in database 
+image_no = zncc.zncc_test(test_array, base_matrix_normalised)
+
+
+#print the match result
+print('The imput image matched the image %s.png in the database' %image_no)
---
+++ CVpro1/zncc.py
@@ -0,0 +1,33 @@
+import base_faces_lib as lib
+import numpy
+
+
+#input_imag:img to be matched   #base_matrix_normalised:image database
+def zncc_test(input_img, base_matrix_normalised):
+	#mean adjust and normalised the imput image
+	input_img = lib.mean_adjustment(input_img)
+	input_img = lib.normalised_vector(input_img)
+
+	array_similarity = numpy.zeros(len(base_matrix_normalised))
+	array_key = numpy.arange(0,len(base_matrix_normalised))
+
+	#normalised the image base matrix
+	for x in range(0, len(base_matrix_normalised)):
+		base_matrix_normalised[x,:] = lib.normalised_vector(base_matrix_normalised[x,:])
+		#print(base_matrix_normalised[x,:])
+		#vector inner product
+		simmilarity = 0;
+		for y in range(0, 10000):
+			simmilarity = simmilarity + input_img[y]*base_matrix_normalised[x,y]
+		array_similarity[x] = simmilarity
+
+
+	#selection sort of the simmilarity array
+	for i in range(len(array_similarity)):
+		swap = i + numpy.argmin(array_similarity[i:])
+		(array_similarity[i], array_similarity[swap]) = (array_similarity[swap], array_similarity[i])
+		(array_key[i], array_key[swap]) = (array_key[swap], array_key[i])
+
+	print(array_similarity[len(array_similarity)-1])
+    
+	return array_key[len(array_key)-1]
