c9d3515c29e3517f53b1b8e655087b5cf6856e72
Allenbi <biji0002@e.ntu.edu.sg>
2019-03-17 14:02:41 +0800

    ¨a¨

---
+++ another_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+from numpy import linalg as la
+
+a = np.array([[2,3,4],[1,2,5]])
+#b = np.resize(a,(2*3,1))
+#c = np.resize(b,(2,3))
+c = np.square(a)
+d = np.sum(c)
+print d
+
